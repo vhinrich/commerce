61ce99d76190ce73e818593f9c71cbab04137926
vhin0210 <vhin0210@gmail.com>
2014-06-27 18:13:05 +0800

    debug js affix

--- sites/all/themes/tobats/js/tobats.affix.js
+++ sites/all/themes/tobats/js/tobats.affix.js
@@ -4,7 +4,10 @@
 			$('.pane-commerce-cart-cart').affix({
 			    offset: {
 			      top: function(){
-			      	return ( $('.pane-system-user-menu').offset().top + $('.pane-system-user-menu').height() );
+			      	if($('.pane-system-user-menu').length>0)
+			      		return ( $('.pane-system-user-menu').offset().top + $('.pane-system-user-menu').height() );
+			      	else
+			      		return ( $('.pane-commerce-cart-cart').offset().top + $('.pane-commerce-cart-cart').height() );
 			      }
 			    }
 			 });
