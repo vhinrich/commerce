80105c52df56a773c57d58dcb8579ddb79d107b4
vhin0210 <vhin0210@gmail.com>
2014-06-27 18:14:29 +0800

    debug affix js

--- sites/all/themes/tobats/js/tobats.affix.js
+++ sites/all/themes/tobats/js/tobats.affix.js
@@ -1,13 +1,14 @@
 (function($){
 	Drupal.behaviors.tobats_affix = {
 		attach: function(context, settings){
+			var initalPosition = ( $('.pane-commerce-cart-cart').offset().top + $('.pane-commerce-cart-cart').height() );
 			$('.pane-commerce-cart-cart').affix({
 			    offset: {
 			      top: function(){
 			      	if($('.pane-system-user-menu').length>0)
 			      		return ( $('.pane-system-user-menu').offset().top + $('.pane-system-user-menu').height() );
 			      	else
-			      		return ( $('.pane-commerce-cart-cart').offset().top + $('.pane-commerce-cart-cart').height() );
+			      		return initalPosition;
 			      }
 			    }
 			 });
